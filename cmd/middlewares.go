d7ddac5778b2a9c645a312d53b9e31329ac3258d
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-21 16:19:21 -0300

    Created a middleware to manage authentication

--- cmd/middlewares.go
+++ cmd/middlewares.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/RamiroCuenca/go-rest-notesApi/auth"
+	"github.com/RamiroCuenca/go-rest-notesApi/common/handler"
+)
+
+// I'm sure that there are some provided by the community
+
+// It's receives and returns a handler
+func AuthenticationMiddleware(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		_, err := auth.ValidateToken(token) // auth is the package we created
+		// If token is invalid
+		if err != nil {
+			forbidden(w, r)
+			return
+		}
+
+		f(w, r)
+	}
+}
+
+func forbidden(w http.ResponseWriter, r *http.Request) {
+	handler.SendResponse(w, http.StatusForbidden, []byte("It hasn't got authorization"))
+}
