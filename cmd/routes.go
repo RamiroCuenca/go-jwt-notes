66c668cf5a312993df21b309638d071a9c6e5393
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-21 17:10:46 -0300

    Added a route for login

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/RamiroCuenca/go-rest-notesApi/notes/controllers"
+	usersControllers "github.com/RamiroCuenca/go-rest-notesApi/users/controllers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -13,12 +14,15 @@ func Routes() *chi.Mux {
 	// Use logger middleware from chi
 	r.Use(middleware.Logger)
 
-	// Handlers
+	// Notes Handlers
 	r.Post("/api/v1/notes/create", controllers.NotesCreate)
 	r.Get("/api/v1/notes/readbyid", controllers.NotesGetById)
 	r.Get("/api/v1/notes/readall", controllers.NotesGetAll)
 	r.Put("/api/v1/notes/update", controllers.NotesUpdateById)
 	r.Delete("/api/v1/notes/delete", controllers.NotesDeleteById)
+
+	// Users Handlers
+	r.Post("/api/v1/users/create", usersControllers.UsersLogin)
 
 	return r
 }
