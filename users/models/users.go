e7aaf6c449f1dd7200970754f964c66c2c672e32
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-21 10:43:56 -0300

    Created a model for users and a structure for claim/payload for the JWT

---
+++ users/models/users.go
@@ -0,0 +1,15 @@
+package models
+
+// All users must have a username and a password.
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Claim is the information that will be sent through the JWT
+// In this case we are going to add only the username. The
+// other parameters are going to be automatically added by
+// the library that we are going to use.
+type Claim struct {
+	Username string `json:"username"`
+}
